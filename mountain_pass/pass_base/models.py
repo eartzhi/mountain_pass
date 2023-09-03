de3ebf8033bf4ee4075bb2922a1868fbfde83eea
eartzhi <zhigunovam@gmail.com>
2023-09-03 12:04:28 +0300

    All modela done, migrate

--- mountain_pass/pass_base/models.py
+++ mountain_pass/pass_base/models.py
@@ -1,22 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=12)
+    fam = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    otc = models.CharField(max_length=30)
 
 
 class Coords(models.Model):
     latitude = models.FloatField(max_length=8)
     longitude = models.FloatField(max_length=8)
-    height = models.IntegerField(max_length=4)
+    height = models.IntegerField()
 
 
 class PerevalImages(models.Model):
-    name = models.CharField(max_length=50)
-    photos = models.ImageField('Фото', upload_to='Pass_images',
+    per_image_name = models.TextField()
+    per_image = models.ImageField('Фото', upload_to='Pass_images',
                                blank=True)
 
 
 class PerevalAdded(models.Model):
     BEAUTY_CHOICE = [
-        ('NO_INFO', 'нет информации'),
+        ('NI', 'нет информации'),
         ('PS', 'Перевал'),
         ('TP', 'Вершина'),
         ('GL', 'Ледник'),
@@ -26,7 +33,7 @@ class PerevalAdded(models.Model):
     ]
 
     LEVEL_CHOICE = [
-        ('NO_INFO', 'нет информации'),
+        ('NI', 'нет информации'),
         ('A1', '1А'),
         ('B1', '1Б'),
         ('А2', '2А'),
@@ -35,23 +42,48 @@ class PerevalAdded(models.Model):
         ('В3', '3Б'),
     ]
 
+    STATUS_CHOICE = [
+        ('NEW', 'новый'),
+        ('PEN', 'в работе'),
+        ('ACC', 'принят'),
+        ('REJ', 'отклонен'),
+    ]
+
+    ACTIVITIES_CHOICE = [
+        ('1', 'пешком'),
+        ('2', 'лыжи'),
+        ('3', 'катамаран'),
+        ('4', 'байдарка'),
+        ('5', 'плот'),
+        ('6', 'сплав'),
+        ('7', 'велосипед'),
+        ('8', 'автомобиль'),
+        ('9', 'мотоцикл'),
+        ('10', 'парус'),
+        ('11', 'верхом'),
+    ]
+
     beauty_title = models.CharField(max_length=2, choices=BEAUTY_CHOICE,
-                                    default='NO_INFO')
+                                    default='NI')
     title = models.TextField()
     other_titles = models.TextField()
-    connect = models.TextField()
+    connect = models.TextField(default='')
     add_time = models.DateTimeField(auto_now_add=True)
-    add_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    add_user = models.ForeignKey(Author, on_delete=models.CASCADE)
     coords = models.ForeignKey(Coords, on_delete=models.CASCADE)
     winter = models.CharField(max_length=2, choices=LEVEL_CHOICE,
-                              default='NO_INFO')
+                              default='NI')
     summer = models.CharField(max_length=2, choices=LEVEL_CHOICE,
-                              default='NO_INFO')
+                              default='NI')
     autumn = models.CharField(max_length=2, choices=LEVEL_CHOICE,
-                              default='NO_INFO')
+                              default='NI')
     spring = models.CharField(max_length=2, choices=LEVEL_CHOICE,
-                             default='NO_INFO')
+                             default='NI')
     add_image = models.ForeignKey(PerevalImages, on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS_CHOICE, max_length=3)
+    spr_activities_types = models.CharField(max_length=2,
+                                            choices=ACTIVITIES_CHOICE,
+                                            default='1')
 
 
 

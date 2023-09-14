681d586145745a2fa1ba423ae1703ee8dfd8ec96
eartzhi <zhigunovam@gmail.com>
2023-09-14 21:02:28 +0300

    docker complete

--- mountain_pass/mountain_pass/settings.py
+++ mountain_pass/mountain_pass/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-_z5zfi+)1rtybmwhcej61+!mlencyq_2i7he28+u=#tiy43sut
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', 'localhost 127.0.0.1 [::1]' , '127.0.0.1']
 
 
 # Application definition
